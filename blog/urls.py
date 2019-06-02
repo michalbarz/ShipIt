cdcc8047a99b0e01130933a39ee9c2605f6158b3
Michal <Michal_Bar-Zakai@Mentor.com>
2019-06-02 14:50:33 +0300

    Modified views and post_list

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+"""mysite URL Configuration"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/new/', views.post_new, name='post_new'),
+]
